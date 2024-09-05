2346f7dd638e58390026de77afd7004e1f331c1b
yunhu <yunhu.liu@outlook.com>
2024-09-06 09:11:43 +1200

    Initial commit

---
+++ src/main/java/com/luitech/springbootexamples/SpringBootExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.luitech.springbootexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luitech/springbootexamples/SpringBootExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luitech.springbootexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
