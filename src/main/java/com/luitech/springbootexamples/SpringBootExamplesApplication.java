27e397563ca50efde50211f46be68ca9526d8176
yunhu <yunhu.liu@outlook.com>
2024-09-21 10:12:03 +1200

    Use addViewControllers to handle home page

--- src/main/java/com/luitech/springbootexamples/SpringBootExamplesApplication.java
+++ src/main/java/com/luitech/springbootexamples/SpringBootExamplesApplication.java
@@ -2,12 +2,18 @@ package com.luitech.springbootexamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringBootExamplesApplication {
+public class SpringBootExamplesApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootExamplesApplication.class, args);
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
 }
