d27a92258f8d5746e7cd934c9f5eb25c33ed6086
yunhu <yunhu.liu@outlook.com>
2024-10-08 22:41:39 +1300

    change back to jpa

--- src/main/java/com/luitech/springbootexamples/domain/Taco.java
+++ src/main/java/com/luitech/springbootexamples/domain/Taco.java
@@ -1,15 +1,20 @@
 package com.luitech.springbootexamples.domain;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private Date createdAt = new Date();
 
@@ -17,7 +22,8 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
+    @NotNull
+    @ManyToMany
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients = new ArrayList<>();
-
+    private List<Ingredient> ingredients;
 }
