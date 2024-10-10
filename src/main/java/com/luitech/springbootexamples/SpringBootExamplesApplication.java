5f0eaab31492494b4334509a30377d2b8fd37ce7
yunhu <yunhu.liu@outlook.com>
2024-10-10 21:34:26 +1300

    add default user

--- src/main/java/com/luitech/springbootexamples/SpringBootExamplesApplication.java
+++ src/main/java/com/luitech/springbootexamples/SpringBootExamplesApplication.java
@@ -1,12 +1,15 @@
 package com.luitech.springbootexamples;
 
 import com.luitech.springbootexamples.data.IngredientRepository;
+import com.luitech.springbootexamples.data.UserRepository;
 import com.luitech.springbootexamples.domain.Ingredient;
 import com.luitech.springbootexamples.domain.IngredientType;
+import com.luitech.springbootexamples.domain.User;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,18 +27,31 @@ public class SpringBootExamplesApplication implements WebMvcConfigurer {
     }
 
     @Bean
-    public ApplicationRunner dataLoader(IngredientRepository repo) {
+    public ApplicationRunner dataLoader(IngredientRepository ingredientRepo, UserRepository userRepo, PasswordEncoder passwordEncoder) {
         return args -> {
-            repo.save(new Ingredient("FLTO", "Flour Tortilla", IngredientType.WRAP));
-            repo.save(new Ingredient("COTO", "Corn Tortilla", IngredientType.WRAP));
-            repo.save(new Ingredient("GRBF", "Ground Beef", IngredientType.PROTEIN));
-            repo.save(new Ingredient("CARN", "Carnitas", IngredientType.PROTEIN));
-            repo.save(new Ingredient("TMTO", "Diced Tomatoes", IngredientType.VEGGIES));
-            repo.save(new Ingredient("LETC", "Lettuce", IngredientType.VEGGIES));
-            repo.save(new Ingredient("CHED", "Cheddar", IngredientType.CHEESE));
-            repo.save(new Ingredient("JACK", "Monterrey Jack", IngredientType.CHEESE));
-            repo.save(new Ingredient("SLSA", "Salsa", IngredientType.SAUCE));
-            repo.save(new Ingredient("SRCR", "Sour Cream", IngredientType.SAUCE));
+            // Existing ingredient data loading
+            ingredientRepo.save(new Ingredient("FLTO", "Flour Tortilla", IngredientType.WRAP));
+            ingredientRepo.save(new Ingredient("COTO", "Corn Tortilla", IngredientType.WRAP));
+            ingredientRepo.save(new Ingredient("GRBF", "Ground Beef", IngredientType.PROTEIN));
+            ingredientRepo.save(new Ingredient("CARN", "Carnitas", IngredientType.PROTEIN));
+            ingredientRepo.save(new Ingredient("TMTO", "Diced Tomatoes", IngredientType.VEGGIES));
+            ingredientRepo.save(new Ingredient("LETC", "Lettuce", IngredientType.VEGGIES));
+            ingredientRepo.save(new Ingredient("CHED", "Cheddar", IngredientType.CHEESE));
+            ingredientRepo.save(new Ingredient("JACK", "Monterrey Jack", IngredientType.CHEESE));
+            ingredientRepo.save(new Ingredient("SLSA", "Salsa", IngredientType.SAUCE));
+            ingredientRepo.save(new Ingredient("SRCR", "Sour Cream", IngredientType.SAUCE));
+
+            // Adding a default user
+            userRepo.save(new User(
+                "admin",
+                passwordEncoder.encode("password"),
+                "Admin User",
+                "123 Admin St",
+                "Admin City",
+                "AD",
+                "12345",
+                "123-456-7890"
+            ));
         };
     }
 }
